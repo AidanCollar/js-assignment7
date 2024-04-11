6a9f25f65820d7f78ef67af44f9028f92f9f9d41
AidanCollar <300088221@fvtc.edu>
2024-04-11 13:50:37 -0500

    Finished the assigment

--- app.js
+++ app.js
@@ -20,11 +20,29 @@ app.get('/', (_, response) => {
 
 
 // GET /api/todos
-
+app.get('/api/todos',(_,response)=>{
+	const itemArray = todos.map(todo => todo.item);
+    response.send(itemArray);
+})
 // POST /api/todos
+app.post('/api/todos',(request,response)=>{
+	const { item } = request.body
+	const id = todos.length + 1
+	const complete = false
+	todos.push({ id, item, complete })
+	response.json('The new id is '+id)
 
+}
+)
 // PUT /api/todos/:id
+app.put('/api/todos/:id',(request,response)=>{
+	const { id } = request.params
+	const task = todos.find(todo => todo.id.toString() === id)
+	task.complete = !task.complete // toggle the complete property
+	const itemArray = todos.map(todo => `Task| ${todo.item} | Is it Complete? ${todo.complete}`);
+    response.send(itemArray);
 
+})
 
 
 const message = `Server running: http://localhost:${port}`
